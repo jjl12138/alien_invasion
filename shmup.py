1314af415781b9f78aad7154fe8ae4e9eeb60897
jjl12138 <2516638426@qq.com>
2020-04-14 16:33:37 +0800

    python飞机大战_1：控制飞机

---
+++ shmup.py
@@ -0,0 +1,87 @@
+# Shmup game - part 1
+# shmup.py
+# Play sprite and movement
+import pygame
+import random
+
+
+WIDTH = 480  # width of our game window
+HEIGHT = 600  # height of our game window
+FPS = 60  # 60 frames per second
+
+
+# Colors(R,G,B),define color
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+
+
+# initialize pygame and create windw
+pygame.init()  # 启动pygame并初始化
+pygame.mixer.init()  # 声音初始化
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # 游戏屏幕，按照在配置常量中设置的大小创建
+pygame.display.set_caption("Shmup!")
+clock = pygame.time.Clock()  # 创建一个时钟以便于确保游戏能以指定的FPS运行
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        # any code here will happen every time the game loop updates
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+
+# Game Loop
+running = True
+
+
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+
+    # Process input(events)    # 这是游戏主循环，通过变量running控制，如果需要
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # Update                   # 游戏结束的话直接将running设为False即可
+    all_sprites.update()
+
+
+    # Render(draw)             # 现在还没有确定具体的代码，先用一些基本代码填充，后续再补充
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # *after* drawing everything,flip the display
+    pygame.display.flip()
+
+
+
+pygame.quit()
